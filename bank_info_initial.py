8aff564cd75d027c78e95a2b8fc72ea4ade783aa
Sude Tıraş <sude.tiras@sabanciuniv.edu>
2024-11-28 13:51:45 +0300

    Preprocessed the bank-info data
    added the labels to the category column

--- bank_info_initial.py
+++ bank_info_initial.py
@@ -1,12 +1,32 @@
 import pandas as pd
 
-# Replace with your raw GitHub file URL
-url = "https://github.com/sudentrs/CS210-Data-Science-Term-Project/blame/main/datasets/bank_info.xlsx"
+# Replace with your local file path
+path = "C:/Users/sudet/Downloads/bank_info.xlsx"
 
-df = pd.read_excel(url)
+# Load the Excel file into a DataFrame
+df = pd.read_excel(path)
 
 # Display the first few rows and column names to understand the structure
 print("Column Names:", df.columns)
 print(df.head())
 
+# Filter the DataFrame to include only rows with negative amounts
+df_expenses = df[df['Amount'] < 0]
+
+# Display the first few rows of the filtered DataFrame
+print(df_expenses.head())
+
+unique_descriptions = df_expenses['Description'].unique()
+print(unique_descriptions)
+
+# Create an empty dictionary to store the labels
+description_labels = {}
+
+# Iterate through each unique description and prompt for a label
+for description in unique_descriptions:
+    label = input(f"Enter label for '{description}': ")
+    description_labels[description] = label
+
+# Display the dictionary with labels
+print(description_labels)
 
